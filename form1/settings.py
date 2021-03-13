4cbf6feb9fef367a0cd15421b066c0c20b89a1c7
Moerle67 <ingo.moerl@posteo.de>
2021-03-13 22:18:12 +0100

    Template1

--- form1/settings.py
+++ form1/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'test1', 
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
---
+++ test1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'test1/index.html')
+
+
